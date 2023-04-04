ecbe0f1deccdf55a30b94f82d3bbf7f914bab301
macdmatt238 <macdmatt238@outlook.com>
2023-04-04 13:25:18 -0400

    fixing remote entry

--- AquaticLazer/src/main/java/Entry/RemoteEntry.java
+++ AquaticLazer/src/main/java/Entry/RemoteEntry.java
@@ -4,7 +4,16 @@
  */
 package Entry;
 
+import com.laserfiche.api.client.model.AccessKey;
+import com.laserfiche.repository.api.RepositoryApiClient;
+import com.laserfiche.repository.api.RepositoryApiClientImpl;
+import com.laserfiche.repository.api.clients.impl.model.ODataValueContextOfIListOfEntry;
 import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.List;
+import java.util.function.Consumer;
 
 /**
  *
@@ -38,7 +47,115 @@ public class RemoteEntry extends Entry{
     
     
     private String JavierFunction(String repositoryID) {
-        return "1";
+       String path=null;
+        int entryid=-48;
+        int idlength=repositoryID.length();
+        for (int i = 0; i < idlength; i++) {
+            char character=repositoryID.charAt(i);
+            entryid+=(int)character;
+        }
+        if (entryid == 1) {
+        } else if (entryid == 4 || entryid == 15 || entryid == 18 || entryid == 23) {
+            String servicePrincipalKey = "9udNhuNq85BwiQW7R_va";
+            String accessKeyBase64 = "ewoJImN1c3RvbWVySWQiOiAiMTQwMTM1OTIzOCIsCgkiY2xpZW50SWQiOiAiOWQwYWMwYTAtZGI4NC00N2RlLWIxZWYtMjI0ZDRiYzZkY2NhIiwKCSJkb21haW4iOiAibGFzZXJmaWNoZS5jYSIsCgkiandrIjogewoJCSJrdHkiOiAiRUMiLAoJCSJjcnYiOiAiUC0yNTYiLAoJCSJ1c2UiOiAic2lnIiwKCQkia2lkIjogImFBU3NaSGFMX1M4bW1qbmVCTkktd3o2c0JBWHFTQmtPWE96V2ZtSjZQbGciLAoJCSJ4IjogImFfTHNINzBoNDVOM0dTUnV4d2RTMmhWSHFyMGcxdGdMVk9wRHV6MjFIRk0iLAoJCSJ5IjogIkpNT1AzQzV1UFNMSFBEUld2TTNTVlVrbjZfNXZkUTJWZTNaaUdTc3I4LTAiLAoJCSJkIjogIlJTS21FeGQxUFBSczBNbnBIUm5QaTNFY2x5VHpCUnJJUnhvVHlGQmhVVWMiLAoJCSJpYXQiOiAxNjc3Mjk3ODY3Cgl9Cn0";
+            String repositoryId = "r-0001d410ba56";
+            AccessKey accessKey = AccessKey.createFromBase64EncodedAccessKey(accessKeyBase64);
+
+            RepositoryApiClient client = RepositoryApiClientImpl.createFromAccessKey(
+                    servicePrincipalKey, accessKey);
+
+            Entry entry = client.getEntriesClient().getEntry(repositoryId, entryid, null).join();
+            path = "C:\\Users\\javie\\Documents\\NetBeansProjects\\Final Project\\newprojecttest\\";
+            path = path + entry.getName();
+            File f1 = new File(path);
+            boolean bool = f1.mkdir();
+            if (bool) {
+                System.out.println("Folder is created successfully");       //you can choose to remove the test but not the if structure
+            } else {
+                System.out.println("Error Found!");
+            }
+            ODataValueContextOfIListOfEntry result = client
+                    .getEntriesClient()
+                    .getEntryListing(repositoryId, entryid, true, null, null, null, null, null, "name", null, null, null).join();
+            List<Entry> entries = result.getValue();
+            for (Entry childEntry : entries) {
+                int entryIdToDownload = childEntry.getId();
+                Entry entry1 = client.getEntriesClient().getEntry(repositoryId, entryIdToDownload, null).join();
+                final String FILE_NAME = entry1.getName() + ".txt";
+                Consumer<InputStream> consumer = inputStream -> {
+                    File exportedFile = new File(f1, FILE_NAME);
+                    try (FileOutputStream outputStream = new FileOutputStream(exportedFile)) {
+                        byte[] buffer = new byte[1024];
+                        while (true) {
+                            int length = inputStream.read(buffer);
+                            if (length == -1) {
+                                break;
+                            }
+                            outputStream.write(buffer, 0, length);
+                        }
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                    } finally {
+                        try {
+                            inputStream.close();
+                        } catch (IOException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                };
+
+                client.getEntriesClient()
+                        .exportDocument(repositoryId, entryIdToDownload, null, consumer)
+                        .join();
+            }
+            client.close();
+            System.out.println("done");
+            return path;
+        } else if (entryid==3||entryid==16||entryid==17||entryid==27||entryid==28||entryid==30||entryid==31||entryid==7||entryid==8||entryid==9||entryid==6||entryid==5||entryid==13||entryid==10||entryid==11||entryid==12||entryid==14||entryid==24||entryid==26||entryid==53||entryid==54||entryid==25||entryid==51||entryid==52){
+            String servicePrincipalKey = "9udNhuNq85BwiQW7R_va";
+            String accessKeyBase64 = "ewoJImN1c3RvbWVySWQiOiAiMTQwMTM1OTIzOCIsCgkiY2xpZW50SWQiOiAiOWQwYWMwYTAtZGI4NC00N2RlLWIxZWYtMjI0ZDRiYzZkY2NhIiwKCSJkb21haW4iOiAibGFzZXJmaWNoZS5jYSIsCgkiandrIjogewoJCSJrdHkiOiAiRUMiLAoJCSJjcnYiOiAiUC0yNTYiLAoJCSJ1c2UiOiAic2lnIiwKCQkia2lkIjogImFBU3NaSGFMX1M4bW1qbmVCTkktd3o2c0JBWHFTQmtPWE96V2ZtSjZQbGciLAoJCSJ4IjogImFfTHNINzBoNDVOM0dTUnV4d2RTMmhWSHFyMGcxdGdMVk9wRHV6MjFIRk0iLAoJCSJ5IjogIkpNT1AzQzV1UFNMSFBEUld2TTNTVlVrbjZfNXZkUTJWZTNaaUdTc3I4LTAiLAoJCSJkIjogIlJTS21FeGQxUFBSczBNbnBIUm5QaTNFY2x5VHpCUnJJUnhvVHlGQmhVVWMiLAoJCSJpYXQiOiAxNjc3Mjk3ODY3Cgl9Cn0";
+            String repositoryId = "r-0001d410ba56";
+            path = "C:\\Users\\javie\\Documents\\NetBeansProjects\\Final Project\\newprojecttest";
+            AccessKey accessKey = AccessKey.createFromBase64EncodedAccessKey(accessKeyBase64);
+
+            RepositoryApiClient client = RepositoryApiClientImpl.createFromAccessKey(
+                    servicePrincipalKey, accessKey);
+
+            int entryIdToDownload = entryid;
+            Entry entry1 = client.getEntriesClient().getEntry(repositoryId, entryIdToDownload, null).join();
+            final String FILE_NAME = entry1.getName() + ".txt";
+            Consumer<InputStream> consumer = inputStream -> {
+                File exportedFile = new File(FILE_NAME);
+                try (FileOutputStream outputStream = new FileOutputStream(exportedFile)) {
+                    byte[] buffer = new byte[1024];
+                    while (true) {
+                        int length = inputStream.read(buffer);
+                        if (length == -1) {
+                            break;
+                        }
+                        outputStream.write(buffer, 0, length);
+                    }
+                } catch (Exception e) {
+                    e.printStackTrace();
+                } finally {
+                    try {
+                        inputStream.close();
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                }
+            };
+
+            client.getEntriesClient()
+                    .exportDocument(repositoryId, entryIdToDownload, null, consumer)
+                    .join();
+
+            client.close();
+            System.out.println("done");
+        }
+        else{
+        }
+        return path;
     }
     
 }
