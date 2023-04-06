e514f3e9ad10dabc16ac96d781b0664b2b7cdef6
macdmatt238 <macdmatt238@outlook.com>
2023-04-06 10:03:05 -0400

    bug fixes

--- AquaticLazer/src/main/java/Entry/Entries.java
+++ AquaticLazer/src/main/java/Entry/Entries.java
@@ -4,6 +4,7 @@
  */
 package Entry;
 
+import com.mycompany.aquaticlazer.GUI;
 import java.io.*;
 import java.util.ArrayList;
 
@@ -13,7 +14,7 @@ import java.util.ArrayList;
  */
 public class Entries {
     public File file;
-    public File[] directoryContents;
+    public Entries[] directoryContents;
     public String name;
     public Long length;
     public String path;
@@ -21,13 +22,13 @@ public class Entries {
     public String[] contents;
     public int repcount = 0;
     
-    Entries(String name) {
-        this.name = name;
+    Entries() {
+        
     }
 
     void readFileContents() {
         
-        
+        int newLineLocation;
         ArrayList<String> tempAL = new ArrayList<String>();
         StringBuilder sb = new StringBuilder();
         sb.append('\n'); // this is done because then everytime each line starts with a \n character which matters when i begin emptying the string builder
@@ -41,11 +42,21 @@ public class Entries {
         }
         
         while (sb.length() != 0) {
-            tempAL.add(sb.delete(1, sb.indexOf("\n")).toString()); // slowly empties the string builder and adds it to the array list one element at a time
-            // deletes the elements from the second character to the element before the \n character and then converts it to a string
-            sb.deleteCharAt(0); // needs to delete the first element
+            newLineLocation = sb.indexOf("\n", 1);
+            
+            if (newLineLocation == -1) {
+                tempAL.add(sb.delete(0, sb.length()).toString());
+            } else {
+                tempAL.add(sb.delete(1, sb.indexOf("\n", 1)).toString()); // slowly empties the string builder and adds it to the array list one element at a time
+                // deletes the elements from the second character to the element before the \n character and then converts it to a string
+                sb.deleteCharAt(0); // needs to delete the first element
+            }
         }
-        contents = (String[]) tempAL.toArray();
+        contents = new String[tempAL.size()];
+        for (int i = 0; i < tempAL.size(); i++) {
+            contents[i] = tempAL.get(i);
+        }
+        
         reader.close();
         } catch (IOException e) {
             
@@ -53,7 +64,7 @@ public class Entries {
         
     }
     
-    public void print(){};
+    public void print(GUI gui){};
 
 
 
